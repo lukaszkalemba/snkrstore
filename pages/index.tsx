037cb19998fce3859fa30e8e86bef81be63f89cc
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-11-22 19:26:43 +0100

    Convert pages directory files to use typescript

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>SNKRSTORE</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <h1>pages/index.tsx</h1>
+    </div>
+  );
+};
+
+export default Home;
